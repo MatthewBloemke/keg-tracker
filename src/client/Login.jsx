51ce2b6d57a2d480749d5be3ffac46a679bc4a24
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 22:35:29 -0500

    Co-authored-by: Tommy Volk <tvolk131@gmail.com>

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -1,9 +1,24 @@
-import * as React from "react"
+import React, {useEffect} from "react"
 
 const Login = () => {
+  const loginFunction = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({data: {employee_email: "dingus@gmail.com", password: "password"}})
+    }
+    await fetch(`http://localhost:5000/api/employees`, requestOptions)
+      .then(response => {
+        console.log(response.status)
+      })
+  }  
+  useEffect(() => {
+    loginFunction()
+
+  }, [])
   return (
     <div>
-      <h1>Time to login!</h1>
+      <h1>I want to login!</h1>
     </div>
   );
 }
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,8 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import * as React from "react";
 import {render} from "react-dom";
 import Login from "./login";
+
 
 render(<Login/>, document.getElementById("app"))
