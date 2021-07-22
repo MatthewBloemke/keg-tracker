293618c1c91fd5f77e2ffee68da2e462fd7f791a
Matthew <mattbloemke@gmail.com>
2021-07-21 23:59:03 -0500

    finished functionality for login page

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -1,24 +1,47 @@
-import React, {useEffect} from "react"
+import React, {useState} from "react"
+import {useHistory} from 'react-router-dom'
 
 const Login = () => {
-  const loginFunction = async () => {
+  const history = useHistory()
+  const initialFormData = {
+    employee_email: "",
+    password: ""
+  }
+  const [formData, setFormData] = useState({...initialFormData})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const handleChange = ({target}) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value
+    })
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formData)
     const requestOptions = {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({data: {employee_email: "dingus@gmail.com", password: "password"}})
+      body: JSON.stringify({data: formData})
     }
-    await fetch(`http://localhost:5000/api/employees`, requestOptions)
+    await fetch(`http://localhost:5000/api/login`, requestOptions)
       .then(response => {
-        console.log(response.status)
+        if (response.status === 200) {
+          history.push("/dashboard")
+        } else {
+          setErrorMessage({message: "Incorrect username or password"})
+        }
       })
-  }  
-  useEffect(() => {
-    loginFunction()
+  }
 
-  }, [])
   return (
     <div>
-      <h1>I want to login!</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="employee_email">Email</label>
+        <input name="employee_email" id="employee_email" value={formData.employee_email} onChange={handleChange} /><br/>
+        <label htmlFor="password">Password</label>
+        <input name="password" id="password" value={formData.password} onChange={handleChange} /><br/>
+        <button type="submit">Login</button>      
+      </form>
     </div>
   );
 }
