7c734a5200b8d415097ea6631dcd68397584345f
BuildTools <unconfigured@null.spigotmc.org>
2021-07-25 15:20:36 -0500

    currently broken

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import {getKegs, isLoggedIn} from './utils/api'
 
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
 const Dashboard = () => {
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-    const abortController = new AbortController()
-    isLoggedIn(abortController.signal)
+    
+    
+    isLoggedIn()
     const [kegs, setKegs] = useState(null)
-    useEffect(async () => {
-        console.log("why wont this fucking work")
-        await fetch(`${API_BASE_URL}/api/kegs`)
-        .then(response => response.json())
-        .then(data => setKegs(data.data))  
-    }, [])    
+
+    const loadDashboard = async () => {
+        console.log(await getKegs(API_BASE_URL))   
+    }
+    loadDashboard()
+    useEffect(() => {
+        const loadKegs = async () => {
+            // const response = await fetch(`${API_BASE_URL}/api/kegs`)
+            // const result = await response.json()
+            // console.log(result.data)
+            console.log("garbage")
+            const result = await getKegs(API_BASE_URL)
+            console.log("after result garbage")
+            setKegs(result)            
+        }
+        loadKegs()
+    }, [])
     return (
-        <div>
+        <div> 
             <h1>This is the dashboard</h1>
             <p>Total Kegs</p>
             <p>Kegs in house</p>
