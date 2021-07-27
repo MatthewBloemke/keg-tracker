44a9f7c1bbf49d646ab1e829364908cc935706e2
Matthew <mattbloemke@gmail.com>
2021-07-27 15:23:14 -0500

    added more functionality

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {getKegs, isLoggedIn} from './utils/api'
+import {getKegs} from './utils/api'
 import {useHistory} from 'react-router-dom'
 import store from 'store'
 
@@ -13,36 +13,55 @@ const Dashboard = () => {
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
     const [kegs, setKegs] = useState([]) 
     const [returnedKegs, setReturnedKegs] = useState([])
+    const [sixtyDayKegs, setSixtyDayKegs] = useState([])
+    const [onetwentyDayKegs, setOnetwentyDayKegs] = useState([])
+    const [overdueKegs, setOverdueKegs] = useState([])
 
-    const loadDashboard = async () => {
-        await getKegs(API_BASE_URL)
-            // .then(response => {
-            //     const tempKegs = response.data
-            //     for (let i = 0; i < tempKegs.length; i++) {
-            //         if (tempKegs[i])
-            //     }
-            // })
-    }
     useEffect(() => {
-        const loadKegs = async () => {
-            // const response = await fetch(`${API_BASE_URL}/api/kegs`)
-            // const result = await response.json()
-            // console.log(result.data)
-            const result = await getKegs(API_BASE_URL)
-            setKegs(result)            
+        const loadDashboard = async () => {
+            const returnedKegsArr = []
+            const sixtyDayKegArr = []
+            const onetwentyDayKegArr = []
+            const overdueKegArr = []
+            await getKegs(API_BASE_URL)
+                .then(response => {
+                    const tempKegs = response
+                    for (let i = 0; i < tempKegs.length; i++) {
+                        if (tempKegs[i].keg_status === "returned") {
+                            returnedKegsArr.push(tempKegs[i])
+                        } else {
+                            let timeA = new Date();
+                            let timeB = new Date(tempKegs[i].date_shipped)
+                            let timeDifference = timeA.getTime() - timeB.getTime()
+                            let daysDifference = timeDifference/1000/3600/24;
+                            if (daysDifference < 60) {
+                                sixtyDayKegArr.push(tempKegs[i])
+                            } else if (daysDifference < 120) {
+                                onetwentyDayKegArr.push(tempKegs[i])
+                            } else {
+                                overdueKegArr.push(tempKegs[i])
+                            }
+                        }
+                    }
+                    setKegs(response)
+                    setReturnedKegs(returnedKegsArr)
+                    setSixtyDayKegs(sixtyDayKegArr)
+                    setOnetwentyDayKegs(onetwentyDayKegArr)
+                    setOverdueKegs(overdueKegArr)
+                })
         }
-        loadKegs()
+
+        loadDashboard()
     }, [])
     return (
         <div> 
             <h1>This is the dashboard</h1>
             <p>Total Kegs {kegs ? kegs.length : null}</p>
-            <p>Kegs returned </p>
-            <p>Total shipped kegs</p>
-            <p>0-60 Days</p>
-            <p>60-120 Days</p>
-            <p>120+ Days</p>
-            {/* <p>{kegs ? kegs[0].keg_size : null}</p> */}
+            <p>Kegs returned {returnedKegs ? returnedKegs.length : null}</p>
+            <p>Total shipped kegs {returnedKegs ? (kegs.length - returnedKegs.length) : null}</p>
+            <p>0-60 Days {sixtyDayKegs ? sixtyDayKegs.length : null}</p>
+            <p>60-120 Days {onetwentyDayKegs ? onetwentyDayKegs.length : null}</p>
+            <p>120+ Days {overdueKegs ? overdueKegs.length : null}</p>
         </div>
     )
 }
