fd6f5712a7c48e5269ea9075f7b209526edcbd24
BuildTools <unconfigured@null.spigotmc.org>
2021-07-26 21:17:48 -0500

    added new login check

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import {getKegs, isLoggedIn} from './utils/api'
+import {useHistory} from 'react-router-dom'
+import store from 'store'
 
-require('react-dom');
-window.React2 = require('react');
-console.log(window.React1 === window.React2);
 
-const Dashboard = () => {
+const Dashboard = () => {   
+    const history = useHistory()
+    if (!store.get("loggedIn")) {
+        history.push('/login')
+    }
+
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-    
-    
-    isLoggedIn()
-    const [kegs, setKegs] = useState(null)
+    const [kegs, setKegs] = useState([]) 
+    const [returnedKegs, setReturnedKegs] = useState([])
 
     const loadDashboard = async () => {
-        console.log(await getKegs(API_BASE_URL))   
+        await getKegs(API_BASE_URL)
+            // .then(response => {
+            //     const tempKegs = response.data
+            //     for (let i = 0; i < tempKegs.length; i++) {
+            //         if (tempKegs[i])
+            //     }
+            // })
     }
-    loadDashboard()
     useEffect(() => {
         const loadKegs = async () => {
             // const response = await fetch(`${API_BASE_URL}/api/kegs`)
             // const result = await response.json()
             // console.log(result.data)
-            console.log("garbage")
             const result = await getKegs(API_BASE_URL)
-            console.log("after result garbage")
             setKegs(result)            
         }
         loadKegs()
@@ -31,13 +36,13 @@ const Dashboard = () => {
     return (
         <div> 
             <h1>This is the dashboard</h1>
-            <p>Total Kegs</p>
-            <p>Kegs in house</p>
+            <p>Total Kegs {kegs ? kegs.length : null}</p>
+            <p>Kegs returned </p>
             <p>Total shipped kegs</p>
             <p>0-60 Days</p>
             <p>60-120 Days</p>
             <p>120+ Days</p>
-            <p>{kegs ? kegs[0].keg_size : null}</p>
+            {/* <p>{kegs ? kegs[0].keg_size : null}</p> */}
         </div>
     )
 }
