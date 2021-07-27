1f27f39e20c68f83604bd362744b3834459ec178
BuildTools <unconfigured@null.spigotmc.org>
2021-07-26 21:16:42 -0500

    removed unneeded function

--- src/client/utils/api.js
+++ src/client/utils/api.js
@@ -1,17 +1,13 @@
-import {useHistory} from'react-router-dom'
-
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
 
 const fetchJson = async (url, options, onCancel) => {
-    const history = useHistory()
     try {
         const response = await fetch(url, options);
 
         if (response.status=== 204) {
             return null
         } else if (response.status === 401) {
-            history.push("/login")
             return undefined
         }
         const payload = await response.json()
@@ -19,7 +15,6 @@ const fetchJson = async (url, options, onCancel) => {
         if (payload.error) {
             throw payload.error
         }
-        //console.log(payload.data)
         return payload.data;
     } catch (error) {
         if (error.name !== "AbortError") {
@@ -28,16 +23,6 @@ const fetchJson = async (url, options, onCancel) => {
         }
         return onCancel
     }
-}
-
-
-export async function isLoggedIn () {
-    await fetchJson('http://localhost:5000/api/employees', {headers} )
-        .then(response => {
-            if (response===401) {
-                history.push('/login')
-            }
-        })
 }
 
 export async function getKegs(baseUrl) {
