be16a6dcc813b584fd0c698d7b5ef7d8c1cf43f7
BuildTools <unconfigured@null.spigotmc.org>
2021-07-29 13:57:52 -0500

    fixed syntax

--- src/client/utils/api.js
+++ src/client/utils/api.js
@@ -5,26 +5,26 @@ const fetchJson = async (url, options, onCancel) => {
     try {
         const response = await fetch(url, options);
 
-        if (response.status=== 204) {
-            return null
+        if (response.status === 204) {
+            return null;
         } else if (response.status === 401) {
-            return undefined
-        }
-        const payload = await response.json()
+            return undefined;
+        };
+        const payload = await response.json();
 
         if (payload.error) {
-            throw payload.error
-        }
+            throw payload.error;
+        };
         return payload.data;
     } catch (error) {
         if (error.name !== "AbortError") {
-            console.error(error.stack)
+            console.error(error.stack);
             throw error;
-        }
-        return onCancel
-    }
-}
+        };
+        return onCancel;
+    };
+};
 
 export async function getKegs(baseUrl) {
-    return await fetchJson(`${baseUrl}/api/kegs`, {headers}, []) 
-}
+    return await fetchJson(`${baseUrl}/api/kegs`, {headers}, []);
+};
