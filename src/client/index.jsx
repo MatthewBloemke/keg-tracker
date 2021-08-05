e2faba7c16b23cbb470fe0cff932fe4e0128a67f
BuildTools <unconfigured@null.spigotmc.org>
2021-08-04 22:34:56 -0500

    added bootstrap

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import * as React from "react";
 import {render} from "react-dom";
 import App from "./App";
