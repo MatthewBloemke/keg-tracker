0ba9834b1925f637a862e0c611650e6fc1d1b10a
BuildTools <unconfigured@null.spigotmc.org>
2021-08-04 22:35:51 -0500

    finished main layout

--- src/client/layout/Layout.jsx
+++ src/client/layout/Layout.jsx
@@ -1,3 +1,22 @@
 import React from 'react'
+import Routes from './Routes';
+import Menu from './Menu';
+import "./Layout.css"
 
-const layout = () = 
+const Layout = () => {
+    const height = {height: "100vh"}
+    return (
+        <div style={height} className="container-fluid">
+            <div className="row h-100">
+                <div className="col-md-2 side-bar">
+                    <Menu/>
+                </div>
+                <div className="col">
+                    <Routes/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
