e691cbe12c6954f7ca4e1578c3791d890ec58950
MatthewBloemke <mattbloemke@gmail.com>
2021-08-06 20:45:22 -0500

    Added submenu functionality

--- src/client/layout/Menu.jsx
+++ src/client/layout/Menu.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const Menu = () => {
-    console.log(window.location.pathname)
-    const style = {display: 'none'}
+    const onClick = ({target}) => {
+        setPath(target.innerText.toLowerCase())
+    }
+    const buttonStyle = {
+        border: "none",
+        background: "none"
+    }
+    const [path, setPath] = useState(window.location.pathname.slice(1))
+    console.log(path)
     return (
         <nav className="navbar navbar-dark align-items-start p-0">
             <div className="container-fluid d-flex flex-column p-0">
                 <li>
                     <Link to="/">
-                        <button className="btn btn-primary">Home</button>
+                        <button style={buttonStyle}>Home</button>
                     </Link>
                 </li>
                 <li>
                     <Link to="/kegs">
-                        Kegs
+                        <button style={buttonStyle} onClick={onClick}>Kegs</button>
                     </Link>
+                    {path ==="kegs" ? <p>Sub menu</p> : null}
                 </li>
                 <li>
                     <Link to="/distributors">
-                        Distributors
+                        <button style={buttonStyle} onClick={onClick}>Distributors</button>
                     </Link>
+                    {path ==="distributors" ? <p>Sub menu</p> : null}
                 </li>
-                <li style={style}>
+                <li>
                     <Link to="employees">
-                        Employees
+                        <button style={buttonStyle} onClick={onClick}>Employees</button>
                     </Link>
+                    {path ==="employees" ? <p>Sub menu</p> : null}
                 </li>                
             </div>
 
