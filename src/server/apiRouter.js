4a569984314bc11dbd889842063978425f781098
MatthewBloemke <mattbloemke@gmail.com>
2021-08-12 20:39:12 -0500

    restructure

---
+++ src/server/apiRouter.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const apiRouter = express.Router();
+
+const kegsRouter = require("./kegs/kegs.router");
+const distributorsRouter = require("./distributors/distributors.router");
+const authRouter = require("./auth/auth.router");
+const loginRouter = require("./login/login.router");
+const jwt = require("express-jwt");
+
+
+
+
+apiRouter.use("/login", loginRouter); //todo: add an actual way to exclude /login from jwt
+apiRouter.use(jwt({
+    secret: process.env.SECRET,
+    algorithms: ['sha1', 'RS256', 'HS256'],
+    getToken: req => req.cookies.token
+}));
+apiRouter.use("/employees", authRouter);
+apiRouter.use("/kegs", kegsRouter);
+apiRouter.use("/distributors", distributorsRouter);
+
+module.exports = apiRouter;
--- src/server/app.js
+++ src/server/app.js
@@ -5,16 +5,10 @@ const express = require("express");
 const cors = require("cors");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
-
-const kegsRouter = require("./kegs/kegs.router");
-const distributorsRouter = require("./distributors/distributors.router");
-const authRouter = require("./auth/auth.router");
-const loginRouter = require("./login/login.router");
 const cookieParser = require("cookie-parser");
-const jwt = require("express-jwt");
 
 const app = express();
-const apiRouter = express.Router();
+const apiRouter = require('./apiRouter')
 
 app.use(cors());
 app.use(express.json());
@@ -23,16 +17,6 @@ app.use(cookieParser());
 app.get("/bundle.js", (req, res) => {
     res.sendFile(path.join(__dirname, `/out/bundle.js`));
 });
-
-apiRouter.use("/login", loginRouter); //todo: add an actual way to exclude /login from jwt
-apiRouter.use(jwt({
-    secret: process.env.SECRET,
-    algorithms: ['sha1', 'RS256', 'HS256'],
-    getToken: req => req.cookies.token
-}));
-apiRouter.use("/employees", authRouter);
-apiRouter.use("/kegs", kegsRouter);
-apiRouter.use("/distributors", distributorsRouter);
 
 app.use("/api", apiRouter);
 app.use(notFound);
