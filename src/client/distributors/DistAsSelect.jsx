4c754aaa404c295fbce6b592adab475a18201a12
MatthewBloemke <mattbloemke@gmail.com>
2021-08-12 20:37:50 -0500

    created distributor formatting file

---
+++ src/client/distributors/DistAsSelect.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getDistributors } from '../utils/api'
+
+const DistAsSelect = ({dist}) => {
+    let distOptions = [<option key="default">Select distributor</option>]
+    
+    dist.forEach(item => {
+        distOptions.push(
+            <option key={item.distributor_id} value={item.distributor_id}>{item.distributor_name}</option>
+        )                
+    })
+
+    return distOptions
+
+    
+    
+}
+
+export default DistAsSelect;
