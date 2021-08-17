2e7ab56cc699a5289d224af95089844f37491094
MatthewBloemke <mattbloemke@gmail.com>
2021-08-17 17:21:46 -0500

    created shipping component

---
+++ src/client/shippingHistory/Shipping.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { getShippingHistory } from '../utils/api'
+import FormatShipping from './FormatShipping'
+
+const Shipping = ({monthlyOnly}) => {
+    const [shipping, setShipping] = useState([])
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        const loadShippingHistory = async () => {
+            await getShippingHistory(abortController.signal)
+                .then(setShipping)
+        }
+        loadShippingHistory()
+        return () => abortController.abort()
+    }, [])
+
+    return (
+        <div>
+            {monthlyOnly ? <h1>Monthly</h1> : <h1>Daily</h1>}
+            <FormatShipping shippingList={shipping} date={date} monthlyOnly={monthlyOnly}/>            
+        </div>
+
+    )
+}
+
+export default Shipping;
