41a84c5363dcae80a02fad1ba7d7a5d1fb8851a3
MatthewBloemke <mattbloemke@gmail.com>
2021-08-17 17:23:39 -0500

    changed table and seed file

--- src/server/db/migrations/20210522033825_createShippingHistory.js
+++ src/server/db/migrations/20210522033825_createShippingHistory.js
@@ -2,20 +2,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable("shippinghistory", (table) => {
       table.increments("shipping_id").primary();
-      table.date("shipped_date");
-      table.integer("keg_id")
-      table
-        .foreign("keg_id")
-        .references("keg_id")
-        .inTable("kegs")
-      table.integer("distributor_id")
-      table
-        .foreign("distributor_id")
-        .references("distributor_id")
-        .inTable("distributors")
-        .onDelete("SET NULL")
+      table.date("date_shipped");
+      table.integer("keg_name")
+      table.string("distributor_name")
       table.timestamps("true, true")
-      table.string("employee_id")
+      table.string("employee_email")
   })
 };
 
