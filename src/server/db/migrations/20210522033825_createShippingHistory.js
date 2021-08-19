52c59ccc0e57670f33ddea6242382b41aabdc694
MatthewBloemke <mattbloemke@gmail.com>
2021-08-19 17:57:00 -0500

    fixed shippingHistory table

--- src/server/db/migrations/20210522033825_createShippingHistory.js
+++ src/server/db/migrations/20210522033825_createShippingHistory.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("shippinghistory", (table) => {
       table.increments("shipping_id").primary();
       table.date("date_shipped");
-      table.integer("keg_name")
+      table.string("keg_name")
       table.string("distributor_name")
       table.timestamps("true, true")
       table.string("employee_email")
