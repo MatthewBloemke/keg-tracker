822d59345b88298233c508ec39198acb83f28ed2
MatthewBloemke <mattbloemke@gmail.com>
2021-08-19 17:58:26 -0500

    added post route

--- src/server/shippingHistory/shippingHistory.router.js
+++ src/server/shippingHistory/shippingHistory.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const controller = require("./shippingHistory.controller")
 
-router.route("/").get(controller.list)
+router.route("/").get(controller.list).post(controller.create)
 
 module.exports = router;
