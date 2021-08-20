baadb26acd917a4f3e5033848157dfbba5b8c534
MatthewBloemke <mattbloemke@gmail.com>
2021-08-20 01:13:10 -0500

    added verify keg route

--- src/server/kegs/kegs.router.js
+++ src/server/kegs/kegs.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("./kegs.controller");
 
 router.route("/").get(controller.list).post(controller.create)
-
+router.route('/verify').post(controller.verifyKeg)
 router.route("/:kegId").get(controller.read).put(controller.update).delete(controller.destroy)
 
 module.exports = router;
