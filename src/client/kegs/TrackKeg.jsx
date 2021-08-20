bba9a7744612a59e9357239c7f6f0dbcaf575b4c
MatthewBloemke <mattbloemke@gmail.com>
2021-08-20 01:12:05 -0500

    added ability to ship multiple kegs at once

--- src/client/kegs/TrackKeg.jsx
+++ src/client/kegs/TrackKeg.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { createHistory, getDistributors } from '../utils/api';
+import { createHistory, getDistributors, verifyKeg } from '../utils/api';
 import DistAsSelect from '../distributors/DistAsSelect';
+import FormatKegIdList from './FormatKegIdList';
+import CustomInput from '../utils/CustomInput';
 
 //todo: add a way to ship multiple kegs to one company at once, change returned to shipped in keg display
 
@@ -10,7 +12,7 @@ const TrackKeg = () => {
     const day = String(date.getDate())
     const initialFormState = {
         date_shipped: `${date.getFullYear()}-${"0"+month.slice(-2)}-${("0"+day).slice(-2)}`,
-        keg_name: "",
+        keg_name: [],
         distributor_id: "",
         employee_email: localStorage.getItem('user')
 
@@ -18,6 +20,7 @@ const TrackKeg = () => {
 
     const [dist, setDist] = useState([])
     const [formData, setFormData] = useState(initialFormState);
+    const [kegName, setKegName] = useState("")
 
     const handleChange = ({target}) => {
         setFormData({
@@ -26,10 +29,36 @@ const TrackKeg = () => {
         })
     }
 
+    const handleKegChange = async ({target}) => {
+        setKegName(target.value)
+        if (target.value.length===4) {
+            if (formData.keg_name.includes(target.value)) {
+                console.log("already entered")
+                setKegName("")
+            } else {
+                await verifyKeg({keg_name: target.value})
+                    .then(response => {
+                        if (response.status === 200) {
+                            setFormData({
+                                ...formData,
+                                keg_name: [...formData.keg_name, target.value]
+                            })
+                        }
+                    })
+
+                setKegName("")                
+            }
+
+        }
+        
+        
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        const abortController = new AbortController()
-        createHistory(formData, abortController.signal)
+        console.log(formData)
+        // const abortController = new AbortController()
+        // createHistory(formData, abortController.signal)
     }
 
     useEffect(() => {
@@ -43,22 +72,33 @@ const TrackKeg = () => {
     return (
         <div>
             <h1>Track Kegs</h1>
-            <form onSubmit={handleSubmit}>
+            
                 <div className="row">
-                    <div className="col-md-3">
-                        <label htmlFor="keg_name">Keg Id</label> <br/>
-                        <label htmlFor="distributor_id">Distributor</label> <br/>
-                        <label htmlFor="date_shipped">Date</label>
+                    <div className="col-md-8">
+                        
+                        
+                            <form onSubmit={handleSubmit}>
+                                <div className="row">
+                                <div className="col-md-6">
+                                    <label htmlFor="distributor_id">Distributor</label> <br/>
+                                    <select id="distributor_id" name="distributor_id" onChange={handleChange}><DistAsSelect dist={dist}/></select> <br/>       
+                                </div>
+                                <div className="col-md-6">
+                                    <CustomInput label="Keg Id" color="blue" name="keg_name"  type="text" handleChange={handleKegChange} value={kegName}/> <br/>
+                                    <CustomInput label="date" color="blue" type="date" name="date_shipped" handleChange={handleChange} value={formData.date_shipped}/>
+                                </div>
+                                <button type="submit">Submit</button>
+                                </div>
+                            </form>
+                        
                     </div>
-                    <div className="col-md-3">
-                        <input name="keg_name" id="keg_name" type="text" onChange={handleChange} value={formData.keg_name}/> <br/>
-                        <select id="distributor_id" name="distributor_id" onChange={handleChange}><DistAsSelect dist={dist}/></select> <br/>
-                        <input type="date" name="date_shipped" id="date_shipped" onChange={handleChange} value={formData.date_shipped}/>
+                    <div className="col-md-4">
+                        <FormatKegIdList kegIds={formData.keg_name}/>
                     </div>
-                    <button type="submit">Submit</button>
+                    
                 </div>
-
-            </form>
+            
+            
         </div>
     )
 }
