a3f09119d63966d7a116eb96248d49bc73d4ccb0
MatthewBloemke <mattbloemke@gmail.com>
2021-08-20 19:31:42 -0500

    added semicolons

--- src/server/shippingHistory/shippingHistory.controller.js
+++ src/server/shippingHistory/shippingHistory.controller.js
@@ -1,26 +1,35 @@
-const service = require('./shippingHistory.service')
-const asyncErrorBoundary = require('../errors/asyncErrorBoundary')
-const {distributorExists} = require('../distributors/distributors.controller')
-const {kegExists} = require("../kegs/kegs.controller")
+const service = require('./shippingHistory.service');
+const kegService = require('../kegs/kegs.service');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+const {distributorExists} = require('../distributors/distributors.controller');
 
 const list = async (req, res) => {
-    const shippingHistory = await service.list()
-    res.json({data: shippingHistory})
-}
+    const shippingHistory = await service.list();
+    res.json({data: shippingHistory});
+};
 
-const createHistory = async (req, res) => {
-    const {date_shipped, employee_email, keg_name} = req.body.data
-    const newHistory = {
-        date_shipped,
-        keg_name,
-        employee_email,
-        distributor_name: res.locals.distributor.distributor_name
-    }
-    await service.create(newHistory)
-    res.json({data: newHistory})
-}
+const createHistory = (req, res) => {
+    const {date_shipped, employee_email, keg_name} = req.body.data;
+    keg_name.forEach(async (keg) => {
+        const newHistory = {
+            date_shipped,
+            keg_name: keg,
+            employee_email,
+            distributor_name: res.locals.distributor.distributor_name            
+        };
+        const updatedKeg = {
+            keg_name: keg,
+            date_shipped,
+            keg_status: "shipped",
+            shipped_to: res.locals.distributor.distributor_name
+        };
+        await kegService.update(updatedKeg);
+        await service.create(newHistory);
+    });
+    res.json({data: keg_name})
+};
 
 module.exports = {
     list,
-    create: [asyncErrorBoundary(kegExists), asyncErrorBoundary(distributorExists), createHistory]
-}
+    create: [asyncErrorBoundary(distributorExists), createHistory]
+};
