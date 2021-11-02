52c539ca297972299b9a636f837a7901461ceae7
MatthewBloemke <mattbloemke@gmail.com>
2021-11-01 21:18:34 -0500

    fixed syntax

--- src/server/db/seeds/03-employees.js
+++ src/server/db/seeds/03-employees.js
@@ -1,4 +1,4 @@
-employees = require("./03-employees.json")
+let employees = require("./03-employees.json")
 
 exports.seed = function(knex) {
   return knex.raw("TRUNCATE TABLE employees RESTART IDENTITY CASCADE")
