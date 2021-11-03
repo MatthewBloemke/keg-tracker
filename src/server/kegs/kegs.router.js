4bdfec0af965a9d52493e5f313ff6af9689c96f2
MatthewBloemke <mattbloemke@gmail.com>
2021-11-02 22:09:18 -0500

    moved PUT function

--- src/server/kegs/kegs.router.js
+++ src/server/kegs/kegs.router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const controller = require("./kegs.controller");
 
-router.route("/").get(controller.list).post(controller.create).put(controller.update)
+router.route("/").get(controller.list).post(controller.create)
 router.route('/verify').post(controller.verifyKeg)
-router.route("/:kegName").get(controller.read).delete(controller.destroy)
+router.route("/:kegName").get(controller.read).put(controller.update).delete(controller.destroy)
 
 module.exports = router;
