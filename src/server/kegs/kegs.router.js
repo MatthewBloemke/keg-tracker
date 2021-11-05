d73df982a45cc5ace8caa76d671e0586c8c2f7ee
MatthewBloemke <mattbloemke@gmail.com>
2021-11-04 22:41:21 -0500

    changed params

--- src/server/kegs/kegs.router.js
+++ src/server/kegs/kegs.router.js
@@ -3,6 +3,6 @@ const controller = require("./kegs.controller");
 
 router.route("/").get(controller.list).post(controller.create)
 router.route('/verify').post(controller.verifyKeg)
-router.route("/:kegName").get(controller.read).put(controller.update).delete(controller.destroy)
+router.route("/:kegId").get(controller.read).put(controller.update).delete(controller.destroy)
 
 module.exports = router;
