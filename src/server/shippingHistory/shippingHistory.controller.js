f690be3ef3dcf0bd3fe976225b1199de67d12249
MatthewBloemke <mattbloemke@gmail.com>
2021-11-05 22:58:01 -0500

    added the rest of crud functions

--- src/server/shippingHistory/shippingHistory.controller.js
+++ src/server/shippingHistory/shippingHistory.controller.js
@@ -1,39 +1,106 @@
 const service = require('./shippingHistory.service');
 const kegService = require('../kegs/kegs.service');
+const employeeService = require("../auth/auth.service");
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
-const {distributorExists} = require('../distributors/distributors.controller');
+const distributorService = require('../distributors/distributors.service');
 
 const list = async (req, res) => {
     const shippingHistory = await service.list();
     res.json({data: shippingHistory});
 };
 
-const createHistory = (req, res) => {
-    const {date_shipped, employee_email, keg_name, keg_status} = req.body.data;
-    keg_name.forEach(async (keg) => {
-        const newHistory = {
-            date_shipped,
-            keg_name: keg,
-            employee_email,
-            distributor_name: res.locals.distributor.distributor_name            
-        };
-        const updatedKeg = {
-            keg_name: keg,
-            date_shipped,
-            keg_status,
-            shipped_to: res.locals.distributor.distributor_name
-        };
+const shippingHistoryExists = async (req, res, next) => {
+    let shippingHistory = await service.read(req.params.shippingId)
 
-        await kegService.update(updatedKeg);
-        if (keg_status==="shipped") {
-            await service.create(newHistory);            
-        }
+    if (shippingHistory) {
+        res.locals.shippingHistory = shippingHistory;
+        return next()
+    }
+    next({status: 404, message: `Shipping History ${req.params.shippingId} not found`})
+}
 
-    });
-    res.json({data: keg_name})
+const hasValidFields = async (req, res, next) => {
+
+    if (!req.body.data) {
+        return next({
+            status: 400,
+            message: "data is missing"
+        })
+    }
+
+    const invalidFields = []
+    const {data = {}} = req.body;   
+    
+    if (data.keg_status != 'returned' && data.keg_status != "shipped") invalidFields.push("keg_status")
+    if (!Date.parse(data.date_shipped)) invalidFields.push("date_shipped");
+    if (!parseInt(data.distributor_id)) {
+        invalidFields.push("distributor_id")
+    } else {
+        const distributor = await distributorService.read(data.distributor_id);
+        if (!distributor) invalidFields.push("distributor_id");
+    }
+    if (!parseInt(data.keg_id)) {
+        invalidFields.push("keg_id")
+    } else {
+        const keg = await kegService.read(data.keg_id);
+        if (!keg) invalidFields.push("keg_id");
+    }
+    if (!data.employee_email) {
+        invalidFields.push("employee_email")
+    } else {
+        const user = await employeeService.readByEmail(data.employee_email)
+        if (!user) invalidFields.push("employee_email")
+    }
+    
+    if (invalidFields.length) {
+        return next({
+            status: 400,
+            message: `${invalidFields.join(", ")} missing data`
+        })
+    }
+    next()
+}
+
+const createHistory = async (req, res) => {
+    const {date_shipped, employee_email, keg_id, keg_status, distributor_id} = req.body.data;
+    const newHistory = {
+        date_shipped,
+        keg_id,
+        employee_email,
+        distributor_id,
+        keg_status
+    }
+    const createdHistory = await service.create(newHistory);
+    res.status(201).json({data: createdHistory})
 };
+
+const read = (req, res) => {
+    res.json({data: res.locals.shippingHistory});
+}
+
+const update = async (req, res) => {
+    const {date_shipped, keg_id, employee_email, distributor_id, keg_status} = req.body.data;
+    const updatedHistory = {
+        shipping_id: req.params.shippingId,
+        date_shipped,
+        keg_id,
+        employee_email,
+        distributor_id,
+        keg_status
+    }
+    await service.update(updatedHistory)
+    res.status(200).json({data: updatedHistory})
+}
+
+const destroy = async (req, res) => {
+    await service.destroy(res.locals.shippingHistory.shipping_id)
+    res.sendStatus(200)
+}
 
 module.exports = {
     list,
-    create: [asyncErrorBoundary(distributorExists), createHistory]
+    read: [asyncErrorBoundary(shippingHistoryExists), read],
+    create: [asyncErrorBoundary(hasValidFields), createHistory],
+    update: [asyncErrorBoundary(shippingHistoryExists), asyncErrorBoundary(hasValidFields), update],
+    destroy: [asyncErrorBoundary(shippingHistoryExists), destroy]
 };
