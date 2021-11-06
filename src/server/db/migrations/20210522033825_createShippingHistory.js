6491c7393ea8c17f10d83700fb80dcdaf3687af5
MatthewBloemke <mattbloemke@gmail.com>
2021-11-05 22:56:56 -0500

    added foreign keys and changed columns

--- src/server/db/migrations/20210522033825_createShippingHistory.js
+++ src/server/db/migrations/20210522033825_createShippingHistory.js
@@ -3,10 +3,23 @@ exports.up = function(knex) {
   return knex.schema.createTable("shippinghistory", (table) => {
       table.increments("shipping_id").primary();
       table.date("date_shipped");
-      table.string("keg_name")
-      table.string("distributor_name")
+      table.integer("keg_id")
+      table
+        .foreign("keg_id")
+        .references("keg_id")
+        .inTable("kegs")
+        .onUpdate("CASCADE")
+        .onDelete("SET NULL")
+      table.integer("distributor_id")
+      table
+        .foreign("distributor_id")
+        .references("distributor_id")
+        .inTable("distributors")
+        .onUpdate("CASCADE")
+        .onDelete("SET NULL")
       table.timestamps("true, true")
       table.string("employee_email")
+      table.string("keg_status")
   })
 };
 
