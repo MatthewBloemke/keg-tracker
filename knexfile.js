f060b0d4443c09a217cc2b7bfac9fe37556a3753
MatthewBloemke <mattbloemke@gmail.com>
2021-11-05 23:39:23 -0500

    fixed env variables

--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,31 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  POSTGRES_HOST_DEVELOPMENT = "localhost",
+  POSTGRES_HOST_DEV = "localhost",
   POSTGRES_HOST_TEST = "localhost",
   POSTGRES_HOST = "localhost",
-  POSTGRES_DEBUG_MODE
+  POSTGRES_USER,
+  POSTGRES_DATABASE,
+  POSTGRES_PASSWORD,
+  POSTGRES_DEBUG_MODE,
+  POSTGRES_USER_TEST,
+  POSTGRES_PASSWORD_TEST,
+  POSTGRES_DATABASE_TEST,
+  POSTGRES_USER_DEV,
+  POSTGRES_PASSWORD_DEV,
+  POSTGRES_DATABASE_DEV,
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: POSTGRES_HOST_DEVELOPMENT,
+    connection: {
+      host: POSTGRES_HOST_DEV,
+      user: POSTGRES_USER_DEV,
+      password: POSTGRES_PASSWORD_DEV,
+      database: POSTGRES_DATABASE_DEV
+    },
     migrations: {
       directory: path.join(__dirname, "src", "server", "db", "migrations"),
     },
@@ -30,7 +44,12 @@ module.exports = {
   test: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: POSTGRES_HOST_TEST,
+    connection: {
+      host: POSTGRES_HOST_TEST,
+      user: POSTGRES_USER_TEST,
+      password: POSTGRES_PASSWORD_TEST,
+      database: POSTGRES_DATABASE_TEST
+    },
     migrations: {
       directory: path.join(__dirname, "src", "server", "db", "migrations"),
     },
@@ -42,7 +61,12 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: POSTGRES_HOST,
+    connection: {
+      host: POSTGRES_HOST,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DATABASE
+    },
     migrations: {
       directory: path.join(__dirname, "src", "server", "db", "migrations"),
     },
