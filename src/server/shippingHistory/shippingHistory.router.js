066af2b09ec28f980a8113372c4f8df091909268
MatthewBloemke <mattbloemke@gmail.com>
2021-11-05 22:58:15 -0500

    added /:shippingId route

--- src/server/shippingHistory/shippingHistory.router.js
+++ src/server/shippingHistory/shippingHistory.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const controller = require("./shippingHistory.controller")
 
 router.route("/").get(controller.list).post(controller.create)
+router.route("/:shippingId").put(controller.update).get(controller.read).delete(controller.destroy)
 
 module.exports = router;
