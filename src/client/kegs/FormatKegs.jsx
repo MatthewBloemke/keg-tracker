1a35f3823264aca0424943d9b976e23b3250fe74
MatthewBloemke <mattbloemke@gmail.com>
2022-01-08 18:44:45 -0600

    modified function to display name rather than id

--- src/client/kegs/FormatKegs.jsx
+++ src/client/kegs/FormatKegs.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
-const FormatKegs = ({kegs, filter}) => {
-    const [filteredKegs, setFilteredKegs] = useState(kegs);
+const FormatKegs = ({kegs, distributors}) => {
     const kegTable = []
+    console.log(distributors)
     kegs.forEach(keg => {
         const path=`/kegs/edit/${keg.keg_id}`
+        console.log(keg)
+        const distributor = distributors.find(({distributor_id}) => distributor_id === keg.shipped_to)
+        console.log(distributor)
         kegTable.push(
             <tr key ={keg.keg_id}>
                 <td>{keg.keg_name}</td>
                 <td>{keg.keg_size}</td>
                 <td>{keg.keg_status}</td>
                 <td>{keg.date_shipped}</td>
-                <td>{keg.shipped_to}</td>
+                <td>{distributor ? distributor.distributor_name : null}</td>
                 <td><Link to={path}><button id={keg.keg_id}>Edit</button></Link></td>
             </tr>
         )
