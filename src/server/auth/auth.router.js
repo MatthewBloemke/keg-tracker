4e7bb2a296970e3b5210cddb33f3a146b63f522b
MatthewBloemke <mattbloemke@gmail.com>
2022-03-12 23:17:55 -0600

    added route for edit, read, and destory

--- src/server/auth/auth.router.js
+++ src/server/auth/auth.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./auth.controller")
 
 router.route("/").get(controller.list).post(controller.createAccount)
+router.route("/:employeeId").get(controller.read).put(controller.update).delete(controller.destroy);
 
 module.exports = router;
