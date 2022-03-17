a6ee70b710192421b4eac972414669839ff1ad21
MatthewBloemke <mattbloemke@gmail.com>
2022-03-16 22:08:32 -0500

    added new routes

--- src/server/auth/auth.router.js
+++ src/server/auth/auth.router.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const controller = require("./auth.controller")
 
 router.route("/").get(controller.list).post(controller.createAccount)
+router.route("/logout").get(controller.logout)
 router.route("/:employeeId").get(controller.read).put(controller.update).delete(controller.destroy);
+router.route("/:employeeId/reset").put(controller.resetPassword)
 
 module.exports = router;
