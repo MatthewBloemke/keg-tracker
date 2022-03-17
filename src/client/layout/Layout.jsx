339b62948ea6c1a0b594fdaf4b916b357e22c0ea
MatthewBloemke <mattbloemke@gmail.com>
2022-03-16 22:07:31 -0500

    abort controller added

--- src/client/layout/Layout.jsx
+++ src/client/layout/Layout.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Routes from './Routes';
 import Menu from './Menu';
 import "./Layout.css"
+import { useHistory } from 'react-router';
+import { loginCheck } from '../utils/api';
 
 const Layout = () => {
+    const history = useHistory();
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+
+    const [pathName, setPathName] = useState(window.location.pathname);
+
+    history.listen((location) => {
+        setPathName(location.pathname)
+    })
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        loginCheck(API_BASE_URL)
+        .then(response => {
+            if (!response) {
+                return () => {
+                    abortController.abort()
+                    history.push("/login")  
+                } 
+            }
+        })
+        return () => abortController.abort()
+    }, [pathName])
     const height = {height: "100vh"}
     return (
         <div style={height} className="container-fluid">
