083840527bfc6e0f0fd2672b9c386214e04f8609
MatthewBloemke <mattbloemke@gmail.com>
2022-03-19 18:10:22 -0500

    changed css

--- src/client/layout/Menu.jsx
+++ src/client/layout/Menu.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 import DistSubMenu from '../distributors/DistSubMenu'
 import EmployeeSubMenu from '../employees/EmployeesSubMenu'
 import KegSubMenu from '../kegs/KegSubMenu'
-import ShippingSubMenu from '../shippingHistory/ShippingSubMenu'
+import ShippingSubMenu from '../shippingHistory/ShippingSubMenu';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 import { logout } from '../utils/api'
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 import "./menu.css"
 
 const Menu = () => {
@@ -31,11 +35,27 @@ const Menu = () => {
         background: "none",
         color: "white"
     }
+
+    const style = {
+        width: '100%',
+        bgcolor: 'background.paper'
+    }
+
     const [path, setPath] = useState(window.location.pathname.slice(1))
     return (
         <nav className="navbar navbar-dark align-items-start p-0">
             <div className="container-fluid d-flex flex-column p-0 listMenu">
-                <li>
+                <List sx={style} component="nav">
+                    <ListItem button>
+                        <ListItemText primary="Home"/>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button divider>
+                        <ListItemText primay="Kegs"/>
+                    </ListItem>
+                    
+                </List>
+                {/* <li>
                     <Link to="/">
                         <button style={buttonStyle} onClick={onClick}>Home</button>
                     </Link>
@@ -70,7 +90,7 @@ const Menu = () => {
                 {path.includes("shipping") ? <ShippingSubMenu/> : null}
                 <li>
                     <button style={buttonStyle} onClick={onLogout}>Logout</button>
-                </li>
+                </li> */}
             </div>
 
         </nav>
