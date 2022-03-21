3b5a67a87d89fc93ca715fdddfa1ef88a5fe4ee0
MatthewBloemke <mattbloemke@gmail.com>
2022-03-20 19:14:58 -0500

    added theme

---
+++ src/client/utils/theme.js
@@ -0,0 +1,14 @@
+import {createTheme} from "@mui/material"
+
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#1875d1",
+        },
+        secondary: {
+            main: "#18d1d1",
+        }
+    }
+})
+
+export default customTheme
