b0b72fc2b7caabc94a1953f90ece545a6482e745
MatthewBloemke <mattbloemke@gmail.com>
2022-03-20 19:16:00 -0500

    changed css

--- src/client/login/Login.jsx
+++ src/client/login/Login.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {useHistory} from 'react-router-dom';
 import { login } from "../utils/api";
 import "./login.css";
-import {TextField, Button} from "@mui/material";
+import {TextField, Button, Container, Box} from "@mui/material";
 import LoginIcon from '@mui/icons-material/Login';
 import Alert from '@mui/material/Alert';
 
@@ -35,23 +35,26 @@ const Login = () => {
   }
 
   return (
-    <main>
-      <div className="header">
-        <p></p>
-        <p></p>
-        <h1>Welcome To Keg Tracker</h1>
-      </div>
-      <div className="form">
-        <form onSubmit={handleSubmit}>
-          <TextField id ="outlined-basic" label="Email" name="employee_email" margin="normal" onChange={handleChange}/> <br/>
-          <TextField id="outlined-password-input" label="Password" type="password" name="password" margin="normal" onChange={handleChange}/> <br/> <br/>
-          <Button type="submit" variant="contained" color="success" size="large" endIcon={<LoginIcon/>} >Login</Button>
-        </form>
-      </div>
-      <div>
-        {errorMessage ? <Alert onClose={() => {setErrorMessage(null)}} sx={{width: "30%", margin: "auto"}} variant="filled" severity="error">{errorMessage.message}</Alert>: null}
-      </div>
-    </main>
+    <Container>
+      <Box sx={{backgroundColor: 'primary'}}>
+        <div className="header">
+          <p></p>
+          <p></p>
+          <h1>Welcome To Keg Tracker</h1>
+        </div>
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <TextField id ="outlined-basic" label="Email" name="employee_email" margin="normal" onChange={handleChange}/> <br/>
+            <TextField id="outlined-password-input" label="Password" type="password" name="password" margin="normal" onChange={handleChange}/> <br/> <br/>
+            <Button type="submit" variant="contained" color="success" size="large" endIcon={<LoginIcon/>} >Login</Button>
+          </form>
+        </div>
+        <div>
+          {errorMessage ? <Alert onClose={() => {setErrorMessage(null)}} sx={{width: "30%", margin: "auto"}} variant="filled" severity="error">{errorMessage.message}</Alert>: null}
+        </div>             
+      </Box>
+    </Container>
+
   );
 }
 
