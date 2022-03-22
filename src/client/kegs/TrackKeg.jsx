a8796e5e74c76e96b736570c8e89603acb54a58f
MatthewBloemke <mattbloemke@gmail.com>
2022-03-22 16:30:48 -0500

    changed to mui

--- src/client/kegs/TrackKeg.jsx
+++ src/client/kegs/TrackKeg.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { createHistory, trackKeg, getDistributors, getKegs, verifyKeg } from '../utils/api';
 import FormatKegIdList from './FormatKegIdList';
-import CustomInput from '../utils/CustomInput';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel';
-import {MenuItem, TextField} from '@mui/material'
+import {MenuItem, TextField, Alert} from '@mui/material'
 import Select from '@mui/material/Select';
+import {LocalizationProvider, DatePicker, } from '@mui/lab'
+import DateFnsUtils from '@mui/lab/AdapterDateFns'
 import "./TrackKeg.css"
 
 //todo: add a way to ship multiple kegs to one company at once, change returned to shipped in keg display
@@ -16,25 +17,20 @@ const TrackKeg = () => {
     const month = String(date.getMonth() + 1)
     const day = String(date.getDate())
     const initialFormState = {
-        date_shipped: `${date.getFullYear()}-${("0"+month).slice(-2)}-${("0"+day).slice(-2)}`,
         keg_id: [],
         employee_email: localStorage.getItem('user'),
         keg_status: "shipped",
     }
+    const initialDate = `${date.getFullYear()}-${("0"+month).slice(-2)}-${("0"+day).slice(-2)}`
 
     const [keg_names, setKeg_names] = useState([]);
     const [distArr, setDistArr] = useState([]);
     const [dist, setDist] = useState("")
+    const [date_shipped, setDate_shipped] = useState(initialDate)
     const [formData, setFormData] = useState(initialFormState);
     const [kegName, setKegName] = useState("");
-
-    const handleChange = (event) => {
-        console.log("change")
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        });
-    };
+    const [alert, setAlert] = useState(null)
+    const [error, setError] = useState(null)
 
     const handleDistChange = (event) => {
         setDist(event.target.value)
@@ -43,20 +39,24 @@ const TrackKeg = () => {
     const handleKegChange = async ({target}) => {
         setKegName(target.value)
         if (target.value.length===4) {
-            if (keg_names.includes(target.value)) {//add an error message here
+            if (keg_names.includes(target.value)) {
+                setError(`Keg ${target.value} has already been added`)
                 setKegName("")
             } else {
                 await verifyKeg({keg_name: target.value})
                     .then(response => {
                         //eventually add if statement for response.keg_status when client is ready for that functionality.
-                        setKeg_names([...keg_names, target.value])
-                        setFormData({
-                            ...formData,
-                            keg_id: [...formData.keg_id, response.keg_id]
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err)
+                        if (response.error) {
+                            setError(response.error)
+                        } else {
+                            setKeg_names([...keg_names, target.value])
+                            setFormData({
+                                ...formData,
+                                keg_id: [...formData.keg_id, response.keg_id]
+                            })                            
+                        }
+                        
+
                     })
                 setKegName("")                
             }
@@ -69,7 +69,7 @@ const TrackKeg = () => {
         const abortController = new AbortController()
         formData.keg_id.forEach( async keg_id => {
             const data = {
-                date_shipped: formData.date_shipped,
+                date_shipped: date_shipped,
                 keg_id,
                 distributor_id: dist,
                 employee_email: formData.employee_email,
@@ -78,6 +78,13 @@ const TrackKeg = () => {
             
             await createHistory(data)
             await trackKeg(data, keg_id, abortController.signal)
+                .then(response => {
+                    if (response.error) {
+                        setError(response.error)
+                    } else {
+                        setAlert("Kegs successfully shipped!")
+                    }
+                })
             setFormData(initialFormState)
             setKeg_names([])
         })
@@ -124,39 +131,50 @@ const TrackKeg = () => {
                     <form onSubmit={handleSubmit}>
                         <div className="row" id="trackKegContainer">
                             <div className="col-md-6" id='distContainer'>
-                                <h3>Choose a Distributor to Ship to</h3> <br/>
+                                <h3 style={{marginTop: "20px"}}>Choose a Distributor to Ship to</h3> <br/>
                                 <Box sx={{ minWidth: 120 }}>
                                     <FormControl sx={{width: "80%"}}>
                                         <InputLabel>Distributor</InputLabel>
                                         <Select
                                             value={dist}
                                             label="Distributor"
-                            
                                             name="distributor_id"
                                             onChange={handleDistChange}
-                                            // sx={{color: 'blue'}}
                                         >
                                             {distArr}
-                                            {/* {dist.map(item => {
-                                                <MenuItem sx={{color:"blue"}} key={item.distributor_id} value={item.distributor_id}>{item.distributor_name}</MenuItem>
-                                            })} */}
                                         </Select>                                        
                                     </FormControl>
                                 </Box>
 
                             </div>
                             <div className="col-md-6" id='inputContainer'>
-                            <TextField id ="outlined-basic" label="Keg Id" name="keg_name" margin="normal" onChange={handleKegChange} value={kegName} disabled={dist ? false : true}/> <br/>
-                                {/* <CustomInput label="Keg Id" color="blue" name="keg_name"  type="text" handleChange={handleKegChange} value={kegName} disabled={dist ? null : "disabled"}/> <br/> */}
-                                <CustomInput label="date" color="blue" type="date" name="date_shipped" handleChange={handleChange} value={formData.date_shipped}/>
+                                <FormControl sx={{width: "50%", marginBottom: "30px"}}>
+                                    <TextField  id ="outlined-basic" label="Keg Id" name="keg_name" margin="normal" onChange={handleKegChange} value={kegName} disabled={dist ? false : true}/> <br/>
+                                    <LocalizationProvider dateAdapter={DateFnsUtils}>
+                                        <DatePicker
+                                            label="Date Shipped"
+                                            value={date_shipped}
+                                            name="date_shipped"
+                                            onChange={(newDate) => {
+                                                setDate_shipped(newDate);
+                                            }}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </LocalizationProvider>
+                                </FormControl>
+                                
+
                             </div>
                         </div>
                         <button className='btn btn-success btn-lg' type="submit">Submit</button>
                     </form>
+                    {error ? <Alert onClose={() => {setError(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="error">{error}</Alert>: null}
+                    {alert ? <Alert onClose={() => {setAlert(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="success">{alert}</Alert>: null}
                 </div>
                 <div className="col-md-4" id='kegContainer'>
                     <FormatKegIdList kegIds={keg_names} onDelete={onDelete}/>
                 </div>
+                
             </div>
         </div>
     )
