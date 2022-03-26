6f0200045c12c63bed9233e68617a03b8dfee84c
MatthewBloemke <mattbloemke@gmail.com>
2022-03-26 15:40:35 -0500

    changed from table to datagrid, updated filter

--- src/client/shippingHistory/FormatShipping.jsx
+++ src/client/shippingHistory/FormatShipping.jsx
@@ -1,54 +1,63 @@
 import React from 'react'
+import {DataGrid} from '@mui/x-data-grid'
 /**
  * TODO: 
  * add buttons to change month and day
  * 
  */
-const FormatShipping = ({date, monthlyOnly, shippingList}) => {
+const FormatShipping = ({date, monthlyOnly, shippingList, kegs, distributors}) => {
+    const abortController = new AbortController();
     const month = date.getMonth()
+    const year = date.getYear()
     const day = date.getDay()
-    const filteredShippingList = []
-    
-    shippingList.forEach((entry) => {
-        const tempMonth = new Date(entry.date_shipped)
+    const shippingListRows = []
+    const columns = [
+        {field: "date_shipped", headerName: "Date Shipped", width: 130},
+        {field: "keg_name", headerName: "Keg Number", width: 130},
+        {field: "distributor_name", headerName: "Distributor", width: 130},
+        {field: "employee_email", headerName: "Employee", width: 150}
+    ]
+    shippingList.forEach(async (entry) => {
+        const current_distributor = distributors.find(({distributor_id}) => distributor_id === entry.distributor_id)
+        const current_keg = kegs.find(({keg_id}) => keg_id === entry.keg_id) 
+        const tempDate = new Date(entry.date_shipped);
+        const tempMonth = String(tempDate.getMonth() + 1);
+        const tempDay = String(tempDate.getDate());
         if (monthlyOnly) {
-            if (tempMonth.getMonth() === month) {
-                filteredShippingList.push(
-                    <tr key={entry.shipping_id}>
-                        <td>{entry.date_shipped.slice(0,10)}</td>
-                        <td>{entry.keg_id}</td>
-                        <td>{entry.distributor_id}</td>
-                        <td>{entry.employee_email}</td>
-                    </tr>   
+            if (tempDate.getMonth() === month && tempDate.getYear() === year) {
+                console.log(tempDate.getYear())
+                shippingListRows.push(
+                    {
+                        id: entry.shipping_id,
+                        date_shipped: `${("0"+tempMonth).slice(-2)}-${("0"+tempDay).slice(-2)}-${tempDate.getFullYear()}`,
+                        keg_name: current_keg ? current_keg.keg_name : null,
+                        distributor_name: current_distributor ? current_distributor.distributor_name : null,
+                        employee_email: entry.employee_email
+                    }
                 )
             }
         } else {
-            if (tempMonth.getMonth() === month && tempMonth.getDay() === day) {
-                filteredShippingList.push(
-                    <tr key={entry.shipping_id}>
-                        <td>{entry.date_shipped.slice(0,10)}</td>
-                        <td>{entry.keg_id}</td>
-                        <td>{entry.distributor_id}</td>
-                        <td>{entry.employee_email}</td>
-                    </tr>   
+            if (tempDate.getMonth() === month && tempDate.getDay() === day && tempDate.getYear() === year) {
+                shippingListRows.push(
+                    {
+                        id: entry.shipping_id,
+                        date_shipped: `${("0"+tempMonth).slice(-2)}-${("0"+tempDay).slice(-2)}-${tempDate.getFullYear()}`,
+                        keg_name: current_keg ? current_keg.keg_name : null,
+                        distributor_name: current_distributor ? current_distributor.distributor_name : null,
+                        employee_email: entry.employee_email
+                    }
                 )
             }
         }
     })
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Keg Id</th>
-                    <th>Distributor</th>
-                    <th>Employee</th>
-                </tr>
-            </thead>
-            <tbody>
-                {filteredShippingList}
-            </tbody>
-        </table>
+        <DataGrid
+            rows={shippingListRows}
+            columns={columns}
+            pageSize={50}
+            rowsPerPageOptions={[50]}
+            sx={{backgroundColor: 'white', width: '70%', height: '100vh'}}
+        />
     )
 }
 
--- src/client/shippingHistory/Shipping.jsx
+++ src/client/shippingHistory/Shipping.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { getShippingHistory } from '../utils/api'
+import { getDistributors, getKegs, getShippingHistory } from '../utils/api'
 import FormatShipping from './FormatShipping'
+import DateFnsUtils from '@mui/lab/AdapterDateFns'
+import {LocalizationProvider, DatePicker, } from '@mui/lab'
+
+import {MenuItem, TextField, Alert, Grid} from '@mui/material'
 //add component for checking how many kegs shipped during timeframe
 
 
 const Shipping = ({monthlyOnly}) => {
     const [shipping, setShipping] = useState([])
     const [date, setDate] = useState(new Date())
+    const [kegs, setKegs] = useState([]);
+    const [distributors, setDistributors] = useState([]);    
 
     useEffect(() => {
         const abortController = new AbortController()
@@ -14,16 +20,60 @@ const Shipping = ({monthlyOnly}) => {
             await getShippingHistory(abortController.signal)
                 .then(setShipping)
         }
+        const loadKegs = async () => {
+            await getKegs(abortController.signal)
+                .then(setKegs)
+        }
+        const loadDistributors = async () => {
+            await getDistributors(abortController.signal)
+                .then(setDistributors)
+        }
         loadShippingHistory()
+        loadKegs()
+        loadDistributors()
         return () => abortController.abort()
     }, [])
 
     return (
-        <div>
-            {monthlyOnly ? <h1>Monthly</h1> : <h1>Daily</h1>}
-            <FormatShipping shippingList={shipping} date={date} monthlyOnly={monthlyOnly}/>            
-        </div>
-
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                {monthlyOnly ? <h1 className="subHeader">Monthly</h1> : <h1 className="subHeader">Daily</h1>}
+            </Grid>
+            <Grid item xs={12} sx={{backgroundColor: 'white', padding: '40px', marginLeft: '24px' }}>
+                {monthlyOnly ?
+                    <LocalizationProvider dateAdapter={DateFnsUtils}>
+                        <DatePicker
+                            views={['year', 'month']}
+                            label="Year and Month"
+                            minDate={new Date('2012-03-01')}
+                            maxDate={new Date('2023-06-01')}
+                            value={date}
+                            onChange={(newDate) => {
+                            setDate(newDate);
+                            }}
+                            renderInput={(params) => <TextField {...params} helperText={null} />}
+                        />                
+                    </LocalizationProvider> 
+                    :
+                    <LocalizationProvider dateAdapter={DateFnsUtils}>
+                        <DatePicker
+                            views={['year', 'month', 'day']}
+                            label="Year, Month, and Date"
+                            minDate={new Date('2012-03-01')}
+                            maxDate={new Date('2023-06-01')}
+                            value={date}
+                            onChange={(newDate) => {
+                            setDate(newDate);
+                            }}
+                            renderInput={(params) => <TextField {...params} helperText={null} />}
+                        />
+                    </LocalizationProvider>
+                }                
+            </Grid>
+            <Grid item xs={12} sx={{backgroundColor: 'white', marginLeft: '24px'}}>
+                <FormatShipping shippingList={shipping} date={date} monthlyOnly={monthlyOnly} kegs={kegs} distributors={distributors}/>            
+            </Grid>
+        </Grid>
     )
 }
 
