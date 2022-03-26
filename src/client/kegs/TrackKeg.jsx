963890c6f25e6cfb0e6d1ae5b59ee48e3bd67e99
MatthewBloemke <mattbloemke@gmail.com>
2022-03-26 15:38:35 -0500

    changed to mui grid instead of bootstrap

--- src/client/kegs/TrackKeg.jsx
+++ src/client/kegs/TrackKeg.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { createHistory, trackKeg, getDistributors, getKegs, verifyKeg } from '../utils/api';
+import { createHistory, trackKeg, getDistributors, verifyKeg } from '../utils/api';
 import FormatKegIdList from './FormatKegIdList';
-import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel';
-import {MenuItem, TextField, Alert} from '@mui/material'
+import {MenuItem, TextField, Alert, Button, Grid} from '@mui/material'
 import Select from '@mui/material/Select';
 import {LocalizationProvider, DatePicker, } from '@mui/lab'
 import DateFnsUtils from '@mui/lab/AdapterDateFns'
@@ -13,25 +12,20 @@ import "./TrackKeg.css"
 //todo: add a way to ship multiple kegs to one company at once, change returned to shipped in keg display
 
 const TrackKeg = () => {
-    const date = new Date()
-    const month = String(date.getMonth() + 1)
-    const day = String(date.getDate())
     const initialFormState = {
         keg_id: [],
         employee_email: localStorage.getItem('user'),
         keg_status: "shipped",
     }
-    const initialDate = `${date.getFullYear()}-${("0"+month).slice(-2)}-${("0"+day).slice(-2)}`
 
     const [keg_names, setKeg_names] = useState([]);
     const [distArr, setDistArr] = useState([]);
     const [dist, setDist] = useState("")
-    const [date_shipped, setDate_shipped] = useState(initialDate)
+    const [date_shipped, setDate_shipped] = useState(new Date(Date.now()))
     const [formData, setFormData] = useState(initialFormState);
     const [kegName, setKegName] = useState("");
     const [alert, setAlert] = useState(null)
     const [error, setError] = useState(null)
-
     const handleDistChange = (event) => {
         setDist(event.target.value)
     }
@@ -45,9 +39,10 @@ const TrackKeg = () => {
             } else {
                 await verifyKeg({keg_name: target.value})
                     .then(response => {
-                        //eventually add if statement for response.keg_status when client is ready for that functionality.
                         if (response.error) {
                             setError(response.error)
+                        } else if (response.keg_status === "shipped") {
+                            setError(`Keg ${response.keg_name} is already shipped.`)
                         } else {
                             setKeg_names([...keg_names, target.value])
                             setFormData({
@@ -67,6 +62,7 @@ const TrackKeg = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         const abortController = new AbortController()
+        date_shipped.setHours(0,0,0,0)
         formData.keg_id.forEach( async keg_id => {
             const data = {
                 date_shipped: date_shipped,
@@ -118,65 +114,72 @@ const TrackKeg = () => {
                 })            
         }
         loadDistributors()
-
-
         return () => abortController.abort()
     }, [])
 
     return (
-        <div>
-            <h1 id='trackKegs'>Track Kegs</h1>
-            <div className="row">
-                <div className="col-md-8">
-                    <form onSubmit={handleSubmit}>
-                        <div className="row" id="trackKegContainer">
-                            <div className="col-md-6" id='distContainer'>
-                                <h3 style={{marginTop: "20px"}}>Choose a Distributor to Ship to</h3> <br/>
-                                <Box sx={{ minWidth: 120 }}>
-                                    <FormControl sx={{width: "80%"}}>
-                                        <InputLabel>Distributor</InputLabel>
-                                        <Select
-                                            value={dist}
-                                            label="Distributor"
-                                            name="distributor_id"
-                                            onChange={handleDistChange}
-                                        >
-                                            {distArr}
-                                        </Select>                                        
-                                    </FormControl>
-                                </Box>
-
-                            </div>
-                            <div className="col-md-6" id='inputContainer'>
-                                <FormControl sx={{width: "50%", marginBottom: "30px"}}>
-                                    <TextField  id ="outlined-basic" label="Keg Id" name="keg_name" margin="normal" onChange={handleKegChange} value={kegName} disabled={dist ? false : true}/> <br/>
-                                    <LocalizationProvider dateAdapter={DateFnsUtils}>
-                                        <DatePicker
-                                            label="Date Shipped"
-                                            value={date_shipped}
-                                            name="date_shipped"
-                                            onChange={(newDate) => {
-                                                setDate_shipped(newDate);
-                                            }}
-                                            renderInput={(params) => <TextField {...params} />}
-                                        />
-                                    </LocalizationProvider>
-                                </FormControl>
-                                
-
-                            </div>
-                        </div>
-                        <button className='btn btn-success btn-lg' type="submit">Submit</button>
-                    </form>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <h1 id='trackKegs' className='subHeader'>Track Kegs</h1>
+            </Grid>
+            <Grid item xs={6}>
+                <Grid container direction="row" justifyContent="center">
+                    <Grid item xs={6}>
+                        <Grid container justifyContent="center">
+                            <h4 style={{marginTop: "20px", marginBottom:"40px"}}>Choose a Distributor to Ship to</h4>
+                            <FormControl sx={{width: "50%"}}>
+                                <InputLabel>Distributor</InputLabel>
+                                <Select
+                                    value={dist}
+                                    label="Distributor"
+                                    name="distributor_id"
+                                    onChange={handleDistChange}
+                                >
+                                {distArr}
+                                </Select>                                        
+                            </FormControl>                            
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Grid container justifyContent="center">
+                            <TextField sx={{marginBottom: '15px'}}  id ="outlined-basic" label="Keg Id" name="keg_name" margin="normal" onChange={handleKegChange} value={kegName} disabled={dist ? false : true}/> <br/>
+                            <FormControl sx={{width: "50%", marginBottom: "30px"}}>
+                                <LocalizationProvider dateAdapter={DateFnsUtils}>
+                                    <DatePicker
+                                        label="Date Shipped"
+                                        value={date_shipped}
+                                        name="date_shipped"
+                                        onChange={(newDate) => {
+                                            setDate_shipped(newDate);
+                                        }}
+                                        renderInput={(params) => <TextField {...params} />}
+                                    />
+                                </LocalizationProvider>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center">
+                            <h5 style={{marginBottom: '20px'}}>Kegs Shipped: {keg_names.length}</h5> <br/>                       
+                        </Grid>
+                        <Grid container justifyContent="center">
+                            <Button onClick={handleSubmit} size="large" variant="contained" color="success">Submit</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={6} >
+                <Grid container direction="row">
+                    <FormatKegIdList kegIds={keg_names} onDelete={onDelete}/>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={1}>
                     {error ? <Alert onClose={() => {setError(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="error">{error}</Alert>: null}
                     {alert ? <Alert onClose={() => {setAlert(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="success">{alert}</Alert>: null}
-                </div>
-                <div className="col-md-4" id='kegContainer'>
-                    <FormatKegIdList kegIds={keg_names} onDelete={onDelete}/>
-                </div>
-                
-            </div>
-        </div>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
