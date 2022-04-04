c191adff79941677d5905f61936356375099d0c4
MatthewBloemke <mattbloemke@gmail.com>
2022-04-03 20:17:17 -0500

    added column to distributors

--- src/server/db/migrations/20210522031823_createDistributors.js
+++ src/server/db/migrations/20210522031823_createDistributors.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("distributors", (table) => {
       table.increments("distributor_id").primary();
       table.string("distributor_name");
+      table.specificType("days_out_arr", "integer ARRAY").defaultTo(null)
       table.timestamps(true, true)
   })
 };
