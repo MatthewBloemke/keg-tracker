902e9f3a6325dd7a90a315ae04aad2eca6933302
MatthewBloemke <mattbloemke@gmail.com>
2022-04-04 19:30:59 -0500

    added grid layout

--- src/client/employees/ListEmployees.jsx
+++ src/client/employees/ListEmployees.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getEmployees} from '../utils/api';
 import FormatEmployeesList from './FormatEmployeesList'
+import {Grid} from '@mui/material'
 
 const ListEmployees = () => {
     const [employees, setEmployees] = useState([]);
@@ -11,7 +12,17 @@ const ListEmployees = () => {
 
         return () => abortController.abort();
     }, []);
-    return <FormatEmployeesList employees={employees}/>
+    return (
+        <Grid>
+            <Grid item xs={12}>
+                <h1 style={{paddingLeft: '10px'}}>Employees</h1>
+            </Grid>
+            <Grid item xs={12}>
+                <FormatEmployeesList employees={employees}/>
+            </Grid>
+        </Grid>
+        
+    )
 }
 
 export default ListEmployees;
--- src/client/employees/NewEmployee.jsx
+++ src/client/employees/NewEmployee.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
 import { createEmployee } from "../utils/api";
+import { Grid, TextField, FormControl, Select, MenuItem, InputLabel, Alert, Button, Stack } from "@mui/material";
+
+//prevent creation of user with duplicate username
 
 const NewEmployee = () => {
-    const history = useHistory()
     const initialFormState = {
         employee_name: "",
         employee_email: "",
@@ -13,8 +14,10 @@ const NewEmployee = () => {
     };
 
     const [formData, setFormData] = useState(initialFormState);
-    const [disabled, setDisabled] = useState("disabled");
-    const [errorMessage, setErrorMessage] = useState(null)
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState(null);
+    const [alert, setAlert] = useState(null);
+    const [passwordError, setPasswordError] = useState(false)
 
     const handleChange = ({target}) => {
         setFormData({
@@ -33,7 +36,7 @@ const NewEmployee = () => {
         if (formData.password != formData.passwordMatch) invalidFields.push("Password");
         if (typeof formData.admin != "boolean") invalidFields.push("Admin")
         if (invalidFields.length) {
-            setErrorMessage(invalidFields.join(", ") + "are invalid")
+            setError(invalidFields.join(", ") + "are invalid")
         } else {
             const data = {
                 employee_email: formData.employee_email,
@@ -42,47 +45,74 @@ const NewEmployee = () => {
                 admin: formData.admin
             };
             console.log(data)
-            await createEmployee(data, abortController.signal);
+            await createEmployee(data, abortController.signal)
+                .then(response => {
+                    if (response.error) {
+                        setError(response.error)
+                    } else {
+                        setAlert("Employee Successfully created")
+                    }
+                });
             setFormData(initialFormState)
-            history.push("/employees")
         }
     };
 
     useEffect(() => {
         if (formData.employee_name && formData.employee_email && formData.password === formData.passwordMatch && formData.password) {
-            setDisabled(null)
+            setDisabled(false)
         } else {
-            setDisabled("disabled")
+            setDisabled(true)
+        }
+        if (formData.passwordMatch != formData.password) {
+            setPasswordError(true)
+        } else {
+            setPasswordError(false)
         }
     }, [formData.employee_email, formData.employee_name, formData.password, formData.passwordMatch])
-
     return (
-        <main>
-            <h1 className="subHeader">New Employee</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-md-3 labels">
-                        <label htmlFor="employee_name">Name</label>
-                        <label htmlFor="employee_email">Email</label>
-                        <label htmlFor="password">Password</label>
-                        <label htmlFor="passwordMatch">Re-type Password</label>
-                        <label htmlFor="admin">Admin?</label>
-                    </div>
-                    <div className="col-md-3 inputs">
-                        <input type="text" id="employee_name" name="employee_name" onChange={handleChange} value={formData.employee_name} />
-                        <input type="text" id="employee_email" name="employee_email" onChange={handleChange} value={formData.employee_email} />
-                        <input type="text" id="password" name="password" onChange={handleChange} value={formData.password} />
-                        <input type="text" id="passwordMatch" name="passwordMatch" onChange={handleChange} value={formData.passwordMatch} />
-                        {formData.password != formData.passwordMatch ? <p>Passwords Do not match</p>: null}
-                        <select name="admin" id="admin" onChange={handleChange}>
-                            <option value={false}>No</option>
-                            <option value={true}>Yes</option>    
-                        </select> <br/>
-                        <button type="submit" disabled={disabled}>Submit</button>
-                    </div>
-                </div>
-            </form>
-        </main>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <h1 style={{paddingLeft: '10px'}}>Create New Employee</h1>
+            </Grid>
+            <Grid item xs={6}>
+                <Stack alignItems="end">
+                    <h5 style={{marginRight: "20px", marginTop: '15px'}}>Name</h5>
+                    <h5 style={{marginRight: "20px", marginTop: '38px'}}>Username</h5>
+                    <h5 style={{marginRight: "20px", marginTop: '38px'}}>Password</h5>
+                    <h5 style={{marginRight: "20px", marginTop: '38px'}}>Retype Password</h5>
+                    <h5 style={{marginRight: "20px", marginTop: '38px'}}>Admin?</h5>
+                </Stack>
+            </Grid>
+            <Grid item xs={6}>
+                    <TextField sx={{width: "40%", marginBottom: '15px'}} id="outlined-basic" label="Name" name="employee_name" onChange={handleChange} value={formData.employee_name} /> <br/>
+                    <TextField sx={{width: "40%", marginBottom: '15px'}} id="outlined-basic" label="Username" name="employee_email" onChange={handleChange} value={formData.employee_email} /> <br/>
+                    <TextField sx={{width: "40%", marginBottom: '15px'}} id="outlined-basic" type="password" label="Password" name="password" onChange={handleChange} value={formData.password} /> <br/>
+                    <TextField sx={{width: "40%", marginBottom: '15px'}} id="outlined-basic" helperText={passwordError ? "Password do not match": null} error = {passwordError} type="password" label="Retype Password" name="passwordMatch" onChange={handleChange} value={formData.passwordMatch} /> <br/>
+                    <FormControl>
+                        <InputLabel>Admin</InputLabel>
+                        <Select
+                            value={formData.admin}
+                            label="Admin"
+                            name="admin"
+                            onChange={handleChange}
+                        >
+                            <MenuItem sx={{ color: "#004a9f"}} value={true}>True</MenuItem>
+                            <MenuItem sx={{ color: "#004a9f"}} value={false}>False</MenuItem>
+                        </Select>
+                    </FormControl>
+            </Grid>
+            <Grid item xs={12} >
+                <Grid container  justifyContent="center" spacing={1}>
+                    <Button onClick={handleSubmit} size="large" variant="contained" disabled={disabled} color="success">Submit</Button> <br/>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} >
+                <Grid container justifyContent="center" spacing={1}>
+                    {error ? <Alert onClose={() => {setError(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="error">{error}</Alert>: null}
+                    {alert ? <Alert onClose={() => {setAlert(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="success">{alert}</Alert>: null}
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
