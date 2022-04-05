0627bc204bde545d1653f6431667e2f4a8a436b8
MatthewBloemke <mattbloemke@gmail.com>
2022-04-04 19:28:41 -0500

    switched to grid layout

--- src/client/distributors/NewDistributor.jsx
+++ src/client/distributors/NewDistributor.jsx
@@ -1,24 +1,23 @@
 import React, {useState} from 'react'
-import { useHistory } from 'react-router-dom';
 import { createDistributor } from "../utils/api"
-
-//add pop up notifying of keg created/ not created
+import {TextField, Alert, Grid, Button} from "@mui/material"
 
 
 const NewDistributor = () => {
-    const history = useHistory()
     const initialFormState = {
         distributor_name: ""
     };
 
     const [formData, setFormData] = useState(initialFormState);
-    const [disabled, setDisabled] = useState("disabled")
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState(null);
+    const [alert, setAlert] = useState(null);
 
     const handleChange = ({target}) => {
         if (target.value.length > 0) {
-            setDisabled(null)
+            setDisabled(false)
         } else {
-            setDisabled("disabled")
+            setDisabled(true)
         }
         setFormData({
             ...formData,
@@ -32,30 +31,46 @@ const NewDistributor = () => {
         const controller = new AbortController();
 
         if (formData.distributor_name.length === 0) {
-            //set an error message to display here
+            setError("Please enter a name")
         } else {
-            await createDistributor(formData, controller.signal);
-            setFormData("initialFormState")
-            history.push("/distributors")
+            await createDistributor(formData, controller.signal)
+                .then(response => {
+                    if (response.error) {
+                        setError(response.error)
+                    } else {
+                        setFormData(initialFormState)
+                        setAlert("Distributor successfully created")                        
+                    }
+                });
+
         }
     }
 
     return (
-        <main>
-            <h1 className="subHeader">New Distributor</h1>
-            <form onSubmit={handleSubmit}>
-                <div className='row'>
-                    <div className='col-md-3 labels'>
-                        <label htmlFor='distributor_name'>Distributor Name</label>
-                    </div>
-                    <div className='col-md-3 inputs'>
-                        <input type="text" id='distributor_name' name="distributor_name" value={formData.distributor_name} onChange={handleChange} /> <br/>
-                        <button type='submit' disabled={disabled}>Submit</button>
-                    </div>
-                </div>
-
-            </form>
-        </main>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <h1 style={{paddingLeft: '10px'}}>Create New Distributor</h1>
+            </Grid>
+            <Grid item xs={6}>
+                <Grid container justifyContent='flex-end'>
+                    <h5 style={{marginTop: '15px'}}>Distributor Name</h5>
+                </Grid>
+            </Grid>
+            <Grid item xs={6}>
+                <TextField sx={{width: "40%"}} id="outlined-basic" label="Distributor Name" name='distributor_name' onChange={handleChange} value={formData.distributor_name} />
+            </Grid>
+            <Grid item xs={12} >
+                <Grid container  justifyContent="center" spacing={1}>
+                    <Button onClick={handleSubmit} size="large" variant="contained" disabled={disabled} color="success">Submit</Button> <br/>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} >
+                <Grid container justifyContent="center" spacing={1}>
+                    {error ? <Alert onClose={() => {setError(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="error">{error}</Alert>: null}
+                    {alert ? <Alert onClose={() => {setAlert(null)}} sx={{width: "40%", margin: "auto", marginTop: "20px"}} variant="filled" severity="success">{alert}</Alert>: null}
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
