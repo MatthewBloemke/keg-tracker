55c648e403dea23f9d349a01b70323019ba166c2
MatthewBloemke <mattbloemke@gmail.com>
2022-04-04 19:33:50 -0500

    added function to update distributors

--- src/server/distributors/distributors.controller.js
+++ src/server/distributors/distributors.controller.js
@@ -11,6 +11,26 @@ async function distributorExists (req, res, next) {
     next({status: 404, message: `Distributor ${req.params.distributorId}`})
 }
 
+const hasValidDaysOut = (req, res, next) => {
+    if (!req.body.data.days_out_arr) {
+        return next({
+            status: 400,
+            message: "days_out_arr is missing"
+        })
+    } else if (!res.locals.distributor.days_out_arr && req.body.data.days_out_arr.length != 1) {
+        return next({
+            status: 400,
+            message: "days_out_arr is not valid"
+        })
+    } else if (res.locals.distributor.days_out_arr) {
+        if (res.locals.distributor.days_out_arr.length != (req.body.data.days_out_arr.length - 1)) {
+            
+            return next({status: 400, message: "days_out_arr not valid"})            
+        }
+    }
+    next()
+}
+
 function hasValidFields (req, res, next) {
     if (!req.body.data) {
         next({
@@ -45,12 +65,15 @@ async function create(req, res) {
 
 async function update(req, res) {
     const {data = {}} = req.body;
-    const newDistributor = {
+    const updatedDistributor = {
         distributor_id: req.params.distributorId,
         distributor_name: data.distributor_name,
     }
-    await service.update(newDistributor)
-    res.status(200).json({data: newDistributor})
+    if (req.body.data.days_out_arr) {
+        updatedDistributor["days_out_arr"] = req.body.data.days_out_arr
+    }
+    await service.update(updatedDistributor)
+    res.status(200).json({data: updatedDistributor})
 }
 
 async function destroy(req, res) {
@@ -63,6 +86,7 @@ module.exports = {
     read: [asyncErrorBoundary(distributorExists), read],
     create: [asyncErrorBoundary(hasValidFields), create],
     update: [asyncErrorBoundary(hasValidFields), asyncErrorBoundary(distributorExists), update],
+    updateDaysOut: [hasValidFields, asyncErrorBoundary(distributorExists), hasValidDaysOut, update],
     destroy: [asyncErrorBoundary(distributorExists), destroy],
     distributorExists,
 }
--- src/server/distributors/distributors.router.js
+++ src/server/distributors/distributors.router.js
@@ -3,5 +3,6 @@ const controller = require("./distributors.controller");
 
 router.route("/").get(controller.list).post(controller.create);
 router.route("/:distributorId").get(controller.read).put(controller.update).delete(controller.destroy);
+router.route("/daysOut/:distributorId").put(controller.updateDaysOut);
 
 module.exports = router;
