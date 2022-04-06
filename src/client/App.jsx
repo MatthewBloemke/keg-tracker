eee6c1032e1a56ba83103c7c306dc584370ec096
MatthewBloemke <mattbloemke@gmail.com>
2022-04-06 13:56:51 -0500

    removed unused imports

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Layout from './layout/Layout'
 import Login from './login/Login'
-import {ThemeProvider} from '@mui/material'
-import {createTheme} from '@mui/material'
-import {blue, teal} from '@mui/material/colors';
 
 
 const App = () => {
