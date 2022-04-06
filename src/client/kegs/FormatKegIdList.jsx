97cf76a0cf757803b8ee7733c5e3393d4cd91823
MatthewBloemke <mattbloemke@gmail.com>
2022-04-05 19:21:19 -0500

    changed to typography

--- src/client/kegs/FormatKegIdList.jsx
+++ src/client/kegs/FormatKegIdList.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import {Fab} from '@mui/material'
+import {Fab, Typography} from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const FormatKegIdList = ({kegIds, onDelete}) => {
     const formattedList = []
     kegIds.forEach(id => {
-        const tempId = id[0].length === 4 ? id[0] + "  " : id + "  ";
+        const tempId = id[0].length === 4 ? id[0]: id;
         formattedList.push(
-            <p style={{backgroundColor: "#E1E2E1"}} key={tempId} className='kegId'>
-                {tempId}
-                <Fab xs={{marginLeft: '10px'}} size='small' aria-label='delete' color='error' name={tempId} onClick={onDelete}><DeleteIcon/></Fab>
-            </p>
+            <Typography key={tempId} variant='h5' component='div'>
+                {tempId + "   "}
+                <Fab size='small' aria-label='delete' color='error' name={tempId} onClick={onDelete}><DeleteIcon/></Fab>
+            </Typography>
+
         )
     })
     return formattedList
